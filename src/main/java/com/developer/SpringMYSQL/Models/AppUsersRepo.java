a32fa97f6c06fb6c602af91d9d9c048d9cae50be
Egor Shumeyko <tierdal@users.noreply.github.com>
2019-10-27 14:32:07 -0500

    Delete AppUsersRepo.java

--- src/main/java/com/developer/SpringMYSQL/Models/AppUsersRepo.java
+++
@@ -1,6 +0,0 @@
-package com.developer.SpringMYSQL.Models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AppUsersRepo extends CrudRepository<AppUsers,Integer> {
-}
